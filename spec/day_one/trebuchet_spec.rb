523b9e8a0af872698135d64a6368108223ef1795
Nikshep A V <nikshep.av@gmail.com>
2023-12-04 10:50:56 +0530

    add failing specs for day 1 - part 2

--- spec/day_one/trebuchet_spec.rb
+++ spec/day_one/trebuchet_spec.rb
@@ -5,5 +5,11 @@ RSpec.describe Trebuchet do
         expect(Trebuchet.solve('./spec/day_one/calibration_input_part_one.txt')).to eq(142)
       end
     end
+
+    context 'for calibration input part two' do
+      it 'returns 281' do
+        expect(Trebuchet.solve('./spec/day_one/calibration_input_part_two.txt')).to eq(281)
+      end
+    end
   end
 end
